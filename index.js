846f220dd9f23b3f5015ef3af9121219659f21b5
Ananya Pathak <anyalko043@gmail.com>
2025-08-28 03:32:27 +0530

    Explore process module and log command-line arguments

--- index.js
+++ index.js
@@ -2,18 +2,26 @@
 // [0] = path to node
 // [1] = path to your script (index.js)
 // [2+] = actual arguements you psas
+const process = require("process");
+// console.log(process.argv);
+const argv = process.argv; 
 
-const args = process.argv.slice(2);
+console.log("number of arguement is "+argv.length);
 
-if(args.includes("-h")){
-  console.log('Usage : node index.js <words>');
-  console.log('Example : node index.js hello world');
-  process.exit(0);
-}
+argv.forEach((val, index) => {
+  console.log(`${index}: ${val}`);
+});
+// const args = process.argv.slice(2);
 
-//Join all arguements into one string
-const input = args.join(" ");
-const wordCount = args.length;
+// if(args.includes("-h")){
+//   console.log('Usage : node index.js <words>');
+//   console.log('Example : node index.js hello world');
+//   process.exit(0);
+// }
 
-console.log(`You entered : "${input}`);
-console.log(`Word Count : ${wordCount}`);
+// //Join all arguements into one string
+// const input = args.join(" ");
+// const wordCount = args.length;
+
+// console.log(`You entered : "${input}`);
+// console.log(`Word Count : ${wordCount}`);
