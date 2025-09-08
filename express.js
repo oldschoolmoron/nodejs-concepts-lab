ed3cc8318d243f6970ca59a9054e6d0200c4b656
Ananya Pathak <anyalko043@gmail.com>
2025-09-09 02:26:09 +0530

    add: basic /sum endpoint for learning

--- express.js
+++ express.js
@@ -5,11 +5,24 @@
 const express = require('express');
 const app = express();
 
-//route handlers 
-app.get('/', function (req , res) {
-  res.send("hello world");
-})
+// //route handlers 
+// app.get('/', function (req , res) {
+//   res.send("hello world");
+// })
 
+function calculateSum(n){
+  let ans = 0;
+  for(let i =1; i<=n ; i++){
+    ans += i;
+  }
+  return ans;
+}
+//127.0.0.1:3000/sum?n=5  || ans = 15
+app.get("/sum", function(req, res){
+  const n = req.query.n;
+  const ans = calculateSum(n);
+  res.send(ans);
+})
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
