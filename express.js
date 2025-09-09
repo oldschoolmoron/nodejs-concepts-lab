d2cecc5169b001a6cdfb40b057279602ebcb3150
Ananya Pathak <anyalko043@gmail.com>
2025-09-09 16:53:29 +0530

    Add lockfile, update package.json, test double query param

--- express.js
+++ express.js
@@ -4,23 +4,38 @@
 
 const express = require('express');
 const app = express();
-
+ 
 // //route handlers 
 // app.get('/', function (req , res) {
 //   res.send("hello world");
 // })
 
-function calculateSum(n){
-  let ans = 0;
-  for(let i =1; i<=n ; i++){
-    ans += i;
-  }
-  return ans;
+// function calculateSum(n){
+//   let ans = 0;
+//   for(let i =1; i<=n ; i++){
+//     ans += i;
+//   }
+//   return ans;
+// }
+// //127.0.0.1:3000/sum?n=5  || ans = 15
+// app.get("/sum", function(req, res){
+//   const n = req.query.n;
+//   const ans = calculateSum(n);
+//   res.send(ans);
+// })
+
+//127.0.0.1:3000/sum?a=5&b=5
+function addTwoNum(a, b){
+  return a + b;
 }
-//127.0.0.1:3000/sum?n=5  || ans = 15
-app.get("/sum", function(req, res){
-  const n = req.query.n;
-  const ans = calculateSum(n);
+
+app.get("/sum", (req, res) => {
+  const a = Number(req.query.a); //s
+  const b = Number(req.query.b);
+  const ans = addTwoNum(a, b);
+  if(isNaN(a,b)) {
+    return res.send("Please provide valid number in query ?a=value1&b=value2")
+  }
   res.send(ans);
 })
 
